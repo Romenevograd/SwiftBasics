212c502257923f3ef9544604cb527a57704e9644
Rimkevich Sergey <Serjrome@yandex.ru>
2024-10-04 00:21:28 +0300

    Prmitive types

---
+++ PrimitiveTypse/PrimitiveTypse/PrimitiveTypseApp.swift
@@ -0,0 +1,43 @@
+//
+//  PrimitiveTypseApp.swift
+//  PrimitiveTypse
+//
+//  Created by Сергей Римкевич on 01.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PrimitiveTypseApp: App {
+    
+    init() {
+        printPrimitiveTypes()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+    func printPrimitiveTypes() {
+        let firstName = "Sergey"
+        
+        //        var lastName = ""
+        //        lastName = "Rimkevich"
+        //        print(firstName)
+        //        print(lastName + " " + firstName)
+        //        print("finish")
+        //
+        //        let dog: Int = 0
+        //        let number: Double = 0.99
+        //
+        //        var array = [1, 2, 3, 4, 5]
+        //        array.append(6)
+        //        array[3] = 999
+        //        array.removeLast()
+        //
+        //        array.forEach { value in
+        //            print(value)
+    }
+}
